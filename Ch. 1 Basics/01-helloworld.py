1e53dd3eba8fa8f4196012d5d98dd3ced88ec187
Myungyon Kim <ceokmy89@gmail.com>
2016-12-15 19:47:28 +0900

    Create 01-helloworld.py

---
+++ Ch. 1 Basics/01-helloworld.py
@@ -0,0 +1,22 @@
+'''
+HelloWorld example using Tensorflow library.
+'''
+
+import tensorflow as tf
+
+# Create a Constant op
+# The op is added as a node to the default graph.
+#
+# The value returned by the constructor represents the output
+# of the Constant op.
+hello = tf.constant('Hello, TensorFlow!')
+
+print(hello)
+
+
+# Start tf session
+sess = tf.Session()
+
+print(sess.run(hello))
+
+# print(sess.run(hello))
